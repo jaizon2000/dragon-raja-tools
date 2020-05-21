895eca8f46e16f9d811c75ad9c65c64713267fba
Jehdi <46204336+jaizon2000@users.noreply.github.com>
2020-05-21 00:17:48 -0600

    Update to img links

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 "use strict";
 
 class Ingredient {
-    constructor(img_name, category) {
-        this.img_src = ".imgs/" + category + "/" + img_name + ".png";
+    constructor(img_name, category, price) {
+        this.name = img_name;
         this.category = category;
+        this.price = price;
     }
+    src() {
+        return "./imgs/" + this.category + "/" + this.name + ".png";
+    }
+}
+
+// const egg = new Ingredient("egg", "meat", 75);
+
+function getsrc(img_name) {
+    const egg = new Ingredient(img_name, "meat", 75);
+    console.log(egg.src());
+    $(this).html();
+    $(this).attr("src", egg.src());
+
 }
