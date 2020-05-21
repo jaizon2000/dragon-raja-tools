f386b442eb51e0f0573b3b0dc8cf0ffb9f71513d
Jehdi <46204336+jaizon2000@users.noreply.github.com>
2020-05-20 23:10:22 -0600

    created Ingredient class

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 "use strict";
+
+class Ingredient {
+    constructor(img_name, category) {
+        this.img_src = ".imgs/" + category + "/" + img_name + ".png";
+        this.category = category;
+    }
+}
